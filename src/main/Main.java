a85da8266cd9ce93fa6fc871885c7e5d7c15d2b1
mikhail <1028880@edu.rut-miit.ru>
2025-11-16 17:48:41 +0300

    added Commit2 method

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,13 @@
 class Main {
     public static void main(String[] args) {
         System.out.println(DefaultMessage());
+        System.out.println(Commit2());
     }
 
     static String DefaultMessage() {
         return "Это изменено в ветке main";
+    }
+    static String Commit2() {
+        return "Коммит #2";
     }
 }
